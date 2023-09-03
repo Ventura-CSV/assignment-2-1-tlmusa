03eb73707b39ea186327e5c8227282343b42cba5
tlmusa <thalia_murguiasanto1@my.vcccd.edu>
2023-09-02 23:54:38 -0700

    done?

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 def main():
+
+    num_males = int(input('Enter number of males: '))
+    num_females = int(input('Enter number of females: '))
+
     """
     ##################################################
-    Comlete your code here
+    Complete your code here
     Use m_perc and f_perc for your results
     ##################################################
     """
+
+    total = num_males + num_females
+    m_perc = total / (float)[num_males]
+    f_perc = total / (float)[num_females]
+
+    print(f'Total: {total}')
+    print(f"Total number of males: \t {m_perc:.2f} Total number of females: \t {f_perc:.2f}")
+
+
 
     """
     ########################################
